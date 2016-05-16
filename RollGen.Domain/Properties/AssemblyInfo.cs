2b7200411bb1c0f5f48158df0a5208be939c4931
Karl Speer <karl.speer@icfi.com>
2016-05-16 16:02:05 -0400

    Release 12.1
    * The NuGet packages in the solution have not been updated, but the
    proper packages have been published.  Will have to update the nuget
    packages in the solution later

--- RollGen.Domain/Properties/AssemblyInfo.cs
+++ RollGen.Domain/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("12.0.*")]
-[assembly: AssemblyFileVersion("12.0.0.0")]
+[assembly: AssemblyVersion("12.1.*")]
+[assembly: AssemblyFileVersion("12.1.0.0")]
 [assembly: InternalsVisibleTo("RollGen.Tests.Integration.IoC")]
 [assembly: InternalsVisibleTo("RollGen.Tests.Unit")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
