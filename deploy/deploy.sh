40823801a7b40dff155eb20038c7704b155f2d1e
Karl Speer <kspeer@globaledit.com>
2018-06-29 15:58:11 -0400

    Nuget update fix
    Issue noted here: https://github.com/travis-ci/travis-ci/issues/5932

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,7 +8,7 @@ echo "Nuget Source is $Source"
 echo "Nuget API Key is $ApiKey (should be secure)"
 
 echo "Packing RollGen"
-nuget pack ./RollGen/RollGen.nuspec -Verbosity detailed
+mono ./nuget.exe pack ./RollGen/RollGen.nuspec -Verbosity detailed
 
 echo "Pushing RollGen"
-nuget push ./DnDGen.RollGen.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
+mono ./nuget.exe push ./DnDGen.RollGen.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
