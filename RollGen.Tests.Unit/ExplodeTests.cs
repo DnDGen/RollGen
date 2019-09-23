6a3ec4a843300287dd0e11832b3e45407c584b34
Liru Færs <lirusaito@gmail.com>
2019-09-23 09:54:49 -0400

    Use Moq<Random> with a sequence instead of custom MockRandom class

--- RollGen.Tests.Unit/ExplodeTests.cs
+++ RollGen.Tests.Unit/ExplodeTests.cs
@@ -1,28 +1,17 @@
 ﻿using Albatross.Expression;
+using Moq;
 using NUnit.Framework;
 using RollGen.Expressions;
 using RollGen.PartialRolls;
 using System;
-using System.Collections.Generic;
 
 namespace RollGen.Tests.Unit
 {
-    // False random class that returns a given array of ints, throws if exceeds array size
-    class MockRandom : Random
-    {
-        private readonly Queue<int> Rolls;
-
-        public MockRandom(int[] rolls) => Rolls = new Queue<int>(rolls);
-
-        public override int Next() => Rolls.Dequeue() - 1;
-        public override int Next(int maxValue) => Next();
-        public override int Next(int minValue, int maxValue) => Next();
-    }
-
     [TestFixture]
     public class ExplodeTests
     {
         readonly ExpressionEvaluator evaluator = new AlbatrossExpressionEvaluator(Factory.Instance.Create());
+        readonly Mock<Random> random = new Mock<Random>();
 
         [TestCase(1, 1, new[]{1, 1}, ExpectedResult = 1)] // 1d1, shouldn't explode
         [TestCase(1, 6, new[]{1}, ExpectedResult = 1)] // Single, no Explode
@@ -34,8 +23,13 @@ namespace RollGen.Tests.Unit
         [TestCase(3, 6, new[]{6, 1, 6, 4, 2}, ExpectedResult = 19)] // Multiple, Explode twice not in a row
         public int ExplodeTest(int quantity, int die, int[] rolls)
         {
-            var random = new MockRandom(rolls);
-            var partialRoll = new NumericPartialRoll(quantity, random, evaluator);
+            var seq = random.SetupSequence(r => r.Next(die));
+            foreach (var roll in rolls)
+            {
+                seq.Returns(roll-1);
+            }
+
+            var partialRoll = new NumericPartialRoll(quantity, random.Object, evaluator);
             partialRoll.d(die).Explode();
 
             return partialRoll.AsSum();
